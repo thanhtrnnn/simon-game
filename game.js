ed094dff6641194bdf5bd25dc7bb18fea973edd3
Thanh Tran <141718531+thanhtrnnn@users.noreply.github.com>
2025-02-01 17:26:31 +0700

    First deployment

---
+++ game.js
@@ -0,0 +1,76 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userPattern = [];
+var level = 0;
+var gameStarted = false, gameOver = false;
+
+$(document).keypress(function() {
+    if (!gameStarted) {
+        nextSequence();
+        gameStarted = true;
+    }
+});
+
+$(".btn").click(function() {
+    if (!gameStarted || gameOver) {
+        return;
+    }
+    var userChosenColor = $(this).attr("id");
+    userPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+    checkAnswer(userPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userPattern[currentLevel]) {
+        if (gamePattern.length === userPattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userPattern = [];
+    gameStarted = false;
+    gameOver = false;
+}
+
+function nextSequence() {
+    userPattern = [];
+    level++;
+    $("#level-title").text(`Level ${level}`);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomColor = buttonColors[randomNumber];
+    gamePattern.push(randomColor);
+
+    $(`#${randomColor}`).fadeOut(100).fadeIn(100);
+    playSound(randomColor);
+}
+
+function animatePress(currentColor) {
+    $(`#${currentColor}`).addClass("pressed");
+    setTimeout(function() {
+        $(`#${currentColor}`). removeClass("pressed");
+    }, 100);
+}
+
+function playSound(color) {
+    var audio = new Audio(`sounds/${color}.mp3`);
+    audio.play();
+}
+
+
